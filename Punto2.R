17b3dd86b5e72c4f3c285838b47e470b9689c98e
Maria Camila Vargas Giraldo <80228169+Camila2301@users.noreply.github.com>
2021-09-22 23:18:55 -0500

    Update Punto2.R

--- Punto2.R
+++ Punto2.R
@@ -4,6 +4,6 @@
 """Ultima actualizacion:22 de septiembre/2021"""
 
 x=6 #Podemos cambiar el valor de la variable si deseamos
-n=5#A continuacion ingresamos los n terminos que vamos a calcular
+n=5 #A continuacion ingresamos los n terminos que vamos a calcular
 V=(((-1)**(1:n))*((x)**(2*1:n+1)))/((2*1:n-1)*(2*1:n))
 1+sum(V)
