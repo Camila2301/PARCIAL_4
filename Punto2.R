2b7af1fcf9709395279c9d7d1f247a39bda3070a
Maria Camila Vargas Giraldo <80228169+Camila2301@users.noreply.github.com>
2021-09-22 23:18:18 -0500

    Add files via upload

---
+++ Punto2.R
@@ -0,0 +1,9 @@
+#El siguiente codigo calcula#
+#Sumatoria del punto D#
+#Autor:Maria Camila Vargas Giraldo#
+#Ultima actualizacion:22 de septiembre/2021#
+
+x=6 #Podemos cambiar el valor de la variable si deseamos
+n=5#A continuacion ingresamos los n terminos que vamos a calcular
+V=(((-1)**(1:n))*((x)**(2*1:n+1)))/((2*1:n-1)*(2*1:n))
+1+sum(V)
